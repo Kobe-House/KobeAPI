b20f5f733274e64e9e9d764a97f4a13632ced485
Tom Amundala <100129520+toma2k@users.noreply.github.com>
2023-11-01 08:59:14 +0200

    Add Scraped Data Into DB

--- database/query/scraping/add/index.php
+++ database/query/scraping/add/index.php
@@ -28,11 +28,36 @@ require '../../../../vendor/autoload.php';
 
  $apiKey = '99f9a6f251cf4da6ab39fcb004ea08c9';
 
+ //UL Variables
+    $dateFirstAvailableUL = '';
+    $placeOfBusinessUL = '';
+    $asinUL = '';
+    $departmentUL = '';
+    $manufacturerUL = '';
+    $itemWeightUL = '';
+    $itemDimensionUL = '';
+    $itemModelNumberUL = '';
+    $sizeUL = '';
+    $colorUL = '';
+    $brandUL = '';
+    $specialFeaturesUL = '';
+
+ //Variable Declaration For Final Data to Insert into the Database
+    $asinFinal = '';
+    $manufacturerFinal = '';
+    $brandFinal = '';
+    $itemWeightFinal = '';
+    $itemDimensionFinal = '';
+    $sizeFinal = '';
+    $widthHeightFinal = '';
+    $ageRangeFinal = '';
+    $colorFinal = '';
+    $specialFeaturesFinal = '';
+    $itemModelNumberFinal = '';
+
 //SCRAPING PROCESS
 if($data->searchText){
 
-   
-    
     //Getting the URL
     $scrapingURL = $data->searchText;
 
@@ -56,6 +81,48 @@ if($data->searchText){
     @$dom->loadHTML($http_response_body);
     $xpath = new DOMXPath($dom);
 
+    // Scenario 1: Check if product details are in a list (ul structure)
+    $elements = $xpath->query('//div[@id="detailBullets_feature_div"]//li//span[@class="a-list-item"]');
+
+    if($elements){
+            foreach ($elements as $element) {
+            $label = $element->getElementsByTagName('span')->item(0)->textContent;
+            $value = $element->getElementsByTagName('span')->item(1)->textContent;
+
+            // Clean up the label and value
+            $label = trim($label);
+            $value = trim($value);
+
+            if (strpos($label, 'Date First Available') !== false) {
+                $dateFirstAvailableUL = $value;
+            } elseif (strpos($label, 'Place of Business') !== false) {
+                $placeOfBusinessUL = $value;
+            } elseif (strpos($label, 'ASIN') !== false) {
+                $asinUL = $value;
+            } elseif (strpos($label, 'Department') !== false) {
+                $departmentUL = $value;
+            } elseif (strpos($label, 'Manufacturer') !== false) {
+                $manufacturerUL = $value;
+            } elseif (strpos($label, 'Brand') !== false) {
+                $brandUL = $value;
+            } elseif (strpos($label, 'Parcel Dimensions') !== false) {
+                $itemDimensionUL = $value;
+            } elseif (strpos($label, 'Model Number') !== false) {
+                $itemModelNumberUL = $value;
+            } elseif (strpos($label, 'Special Features') !== false) {
+                $specialFeaturesUL = $value;
+            } elseif (strpos($label, 'Manufacturer') !== false) {
+                $sizeUL = $value;
+            } elseif (strpos($label, 'Colour') !== false) {
+                $colorUL = $value;
+            }
+            
+        }
+    }else{
+        echo "Product Details Nothing Found";
+    }
+    
+
     //Extracting the product title
     $titleSection = $xpath->query('//div[@id="titleSection"]');
     if($titleSection->length > 0){
@@ -68,30 +135,282 @@ if($data->searchText){
         $imageURL = $xpath->query('.//img/@src', $imagTagWrapper->item(0))->item(0)->nodeValue;
     }
 
-    //Extract the Description
+    
+    // Extract the Description 1st structure
     $featureBullets = $dom->getElementById('feature-bullets');
-    $liItems = $featureBullets->getElementsByTagName('li');
+    $descriptions = array(); 
 
-    $descriptions = array(); // Initialize an array to store the descriptions
+    if ($featureBullets) {
+        $liItems = $featureBullets->getElementsByTagName('li');
+        foreach ($liItems as $liItem) {
+            // Get the first span element
+            $span = $liItem->getElementsByTagName('span')->item(0); 
+            if ($span) {
+                $description = $span->nodeValue;
+                $descriptions[] = $description; 
+            }
+        }
+    }
+    // Extract the Description from the 2nd structure
+    $expander = $dom->getElementById('productFactsDesktopExpander');
 
-    foreach ($liItems as $liItem) {
-        $span = $liItem->getElementsByTagName('span')[0]; // Get the first span element
-        if ($span) {
-            $description = $span->nodeValue;
-            $descriptions[] = $description; // Store the description in the array
+    if ($expander) {
+        $liItems = $expander->getElementsByTagName('li');
+
+        foreach ($liItems as $liItem) {
+            $span = $liItem->getElementsByTagName('span')->item(0);
+            if ($span) {
+                $description = $span->nodeValue;
+                $descriptions[] = $description;
+            }
         }
     }
 
+    //Scenario 2: Scrape description in TABLE like structure
+
+    // Initialize an array to store Table description data
+    $dataForProductDescription = array();
+
+    // Define the sections you want to scrape
+    $sections = array(
+        'Technical Details' => 'productDetails_techSpec_section_1',
+        'Additional Information' => 'productDetails_detailBullets_sections1',
+    );
+
+    // Define the attributes you want to extract
+    $attributes = array(
+        'Manufacturer',
+        'Item model number',
+        'Parcel Dimensions',
+        'ASIN',
+        'Fabric Type',
+        'Place of Business',
+        'Care Instructions',
+        'Batteries Required',
+        'Date First Available',
+        'Item Weight',
+        'Included components',
+        'Maximum Recommended Load',
+        'Size',
+        'Brand',
+        'Special Features',
+    );
+
+    // Loop through the sections and attributes
+    foreach ($sections as $sectionName => $sectionId) {
+        // Find the section by ID
+        $section = $xpath->query("//*[@id='$sectionId']");
+
+        if ($section->length > 0) {
+            // Find the rows within the section
+            $rows = $xpath->query(".//tr", $section->item(0));
+
+            foreach ($rows as $row) {
+
+                // Extract the header and data cells
+                $header = trim($xpath->query(".//th", $row)->item(0)->textContent);
+                $dataCell = trim($xpath->query(".//td", $row)->item(0)->textContent);
+
+                // Check if the header is in the list of attributes to scrape
+                if (in_array($header, $attributes)) {
+                    $dataForProductDescription[$sectionName][$header] = $dataCell;
+                }
+            }
+        }
+    }
+ 
+    //Get information from `dataForProductDescription` array and Prep em to be inserted
+    $technicalDetails = isset($dataForProductDescription['Technical Details']) ? $dataForProductDescription['Technical Details'] : array();
+    $additionalInformation = isset($dataForProductDescription['Additional Information']) ? $dataForProductDescription['Additional Information'] : array();
+
+    // Check and assign values or "N/A"
+    $itemModelNumberTABLE = isset($technicalDetails['Model Number']) ? $technicalDetails['Model Number'] : (
+        isset($additionalInformation['Model Number']) ? $additionalInformation['Model Number'] : ''
+    );
+    $specialFeaturesTABLE = isset($technicalDetails['Special Features']) ? $technicalDetails['Special Features'] : (
+        isset($additionalInformation['Special Features']) ? $additionalInformation['Special Features'] : ''
+    );
+    $brandTABLE = isset($technicalDetails['Brand']) ? $technicalDetails['Brand'] : (
+        isset($additionalInformation['Brand']) ? $additionalInformation['Brand'] :''
+    );
+    $itemDimensionsTABLE = isset($technicalDetails['Parcel Dimensions']) ? $technicalDetails['Parcel Dimensions'] : (
+        isset($additionalInformation['Parcel Dimensions']) ? $additionalInformation['Parcel Dimensions'] : ''
+    );
+
+    $asinTABLE = isset($technicalDetails['ASIN']) ? $technicalDetails['ASIN'] : (
+        isset($additionalInformation['ASIN']) ? $additionalInformation['ASIN'] : ''
+    );
+    
+    $dateFirstAvailableTABLE = isset($technicalDetails['Date First Available']) ? $technicalDetails['Date First Available'] : (
+        isset($additionalInformation['Date First Available']) ? $additionalInformation['Date First Available'] : ''
+    );
+    $manufacturerTABLE = isset($technicalDetails['Manufacturer']) ? $technicalDetails['Manufacturer'] : (
+        isset($additionalInformation['Manufacturer']) ? $additionalInformation['Manufacturer'] : ''
+    );
+    $itemWeightTABLE = isset($technicalDetails['Item Weight']) ? $technicalDetails['Item Weight'] : (
+        isset($additionalInformation['Item Weight']) ? $additionalInformation['Item Weight'] :  ''
+    );
+    $sizeTABLE = isset($technicalDetails['size']) ? $technicalDetails['size'] : (
+        isset($additionalInformation['size']) ? $additionalInformation['size'] :  ''
+    );
+    $colorTABLE = isset($technicalDetails['Colour']) ? $technicalDetails['Colour'] : (
+        isset($additionalInformation['Colour']) ? $additionalInformation['Colour'] :  ''
+    );
+
+    // Extracting Brand Name
+    $brandNameElement = $xpath->query('//tr[contains(@class, "po-brand")]//span[@class="a-size-base po-break-word"]')->item(0);
+    $brandBelowTitle = $brandNameElement ? $brandNameElement->textContent : "N/A";
+
+    // Extracting Model Name
+    $colorElement = $xpath->query('//tr[contains(@class, "po-color")]//span[@class="a-size-base po-break-word"]')->item(0);
+    $colorBelowTitle = $colorElement ? $colorElement->textContent : "N/A";
+
+    // Extracting Special Feature
+    $specialFeatureElement = $xpath->query('//tr[contains(@class, "po-special_feature")]//span[@class="a-size-base po-break-word"]')->item(0);
+    $specialFeatureBelowTitle = $specialFeatureElement ? $specialFeatureElement->textContent : "N/A";
+
+    // Extracting Age Range
+    $ageRangeElement = $xpath->query('//tr[contains(@class, "po-age_range_description")]//span[@class="a-size-base po-break-word"]')->item(0);
+    $ageRangeBelowTitle = $ageRangeElement ? $ageRangeElement->textContent : "N/A";
+
+    // Extracting Width Height
+    $dimensionElement = $xpath->query('//tr[contains(@class, "po-item_depth_width_height")]//span[@class="a-size-base po-break-word"]')->item(0);
+    $widthHeight = $dimensionElement ? $dimensionElement->textContent : "N/A";
+
+    //Final ASIN
+    if (!empty($asinTABLE)) {
+        $asinFinal = $asinTABLE;
+    } elseif (!empty($asinUL)) {
+        $asinFinal = $asinUL;
+    }
+
+    //If No ASIN
+    if (empty($asinFinal)) {
+        $asinFinal = 'N/A';
+    }
+
+    //Final Manufacturer
+    if (!empty($manufacturerTABLE)) {
+        $manufacturerFinal = $manufacturerTABLE;
+    } elseif (!empty($manufacturerUL)) {
+        $manufacturerFinal = $manufacturerUL;
+    }
+
+    //If No Manufacturer
+    if (empty($manufacturerFinal)) {
+        $manufacturerFinal = 'N/A';
+    }
+    //Final Brand Name
+    if (!empty($brandTABLE)) {
+        $brandFinal = $brandTABLE;
+    } elseif (!empty($brandBelowTitle)) {
+        $brandFinal = $brandBelowTitle;
+    }
+
+    //If No Brand Name
+    if (empty($brandFinal)) {
+        $brandFinal = 'N/A';
+    }
+
+    //Final Item Weight
+    if (!empty($itemWeightTABLE)) {
+        $itemWeightFinal = $itemWeightTABLE;
+    } elseif (!empty($brandBelowTitle)) {
+        $itemWeightFinal = $itemWeightUL;
+    }
+
+    //If No Item Weight
+    if (empty($itemWeightFinal)) {
+        $itemWeightFinal = 'N/A';
+    }
+
+    //Final Item Dimension
+    if (!empty($itemDimensionsTABLE)) {
+        $itemDimensionFinal = $itemDimensionUL;
+    } elseif (!empty($itemDimensionUL)) {
+        $itemDimensionFinal = $itemWeightUL;
+    }
+
+    //If No Item Dimension
+    if (empty($itemDimensionFinal)) {
+        $itemDimensionFinal = 'N/A';
+    }
+
+    //Final Item Model NUmber
+    if (!empty($itemModelNumberTABLE)) {
+        $itemModelNumberFinal = $itemModelNumberTABLE;
+    } elseif (!empty($itemModelNumberUL)) {
+        $itemModelNumberFinal = $itemModelNumberUL;
+    }
+
+    //If No Item Model NUmber
+    if (empty($itemModelNumberFinal)) {
+        $itemModelNumberFinal = 'N/A';
+    }
+
+    //Final Item Special Features
+    if (!empty($specialFeaturesTABLE)) {
+        $specialFeaturesFinal = $specialFeaturesTABLE;
+    } elseif (!empty($specialFeaturesUL)) {
+        $specialFeaturesFinal = $specialFeaturesUL;
+    } elseif (!empty($specialFeatureBelowTitle)) {
+        $specialFeaturesFinal = $specialFeatureBelowTitle;
+    }
+
+    //If No Item Special Features
+    if (empty($specialFeaturesFinal)) {
+        $specialFeaturesFinal = 'N/A';
+    }
+
+    //Final Item Color
+    if (!empty($colorTABLE)) {
+        $colorFinal = $colorTABLE;
+    } elseif (!empty($colorUL)) {
+        $colorFinal = $colorUL;
+    } elseif (!empty($colorBelowTitle)) {
+        $colorFinal = $colorBelowTitle;
+    }
+
+    //If No Item Color
+    if (empty($colorFinal)) {
+        $colorFinal = 'N/A';
+    }
+    
+    //Final Item Size
+    if (!empty($sizeTABLE)) {
+        $sizeFinal = $sizeTABLE;
+    } elseif (!empty($sizeUL)) {
+        $sizeFinal = $sizeUL;
+    }
+
+    //If No Item Size
+    if (empty($sizeFinal)) {
+        $sizeFinal = 'N/A';
+    }
+
+    //SANITATION
+
+    $asinFinal = $mysqli->real_escape_string($asinFinal);
+    $manufacturerFinal = $mysqli->real_escape_string($manufacturerFinal);
+    $brandFinal = $mysqli->real_escape_string($brandFinal);
+    $itemWeightFinal = $mysqli->real_escape_string($itemWeightFinal);
+    $itemDimensionFinal = $mysqli->real_escape_string($itemDimensionFinal);
+    $itemModelNumberFinal = $mysqli->real_escape_string($itemModelNumberFinal);
+    $specialFeaturesFinal = $mysqli->real_escape_string($specialFeaturesFinal);
+    $sizeFinal = $mysqli->real_escape_string($sizeFinal);
+    $colorFinal = $mysqli->real_escape_string($colorFinal);
+
     //INSERT INTO THE DTABASE
-    $sql = "INSERT INTO `product` (`title`, `image_url`, `url`, `created_at`) 
-            VALUES ('$productTitle', '$imageURL', '$scrapingURL', now())";
+    $sql = "INSERT INTO `product` (`title`, `image_url`, `url`, `created_at`, `item_model`, `parcel_dimensions`, `asin`, `manufacturer`, `item_weight`, `size`, `special_features`, `color`, `brand`) 
+            VALUES ('$productTitle', '$imageURL', '$scrapingURL', now(), '$itemModelNumberFinal', '$itemDimensionFinal', '$asinFinal', '$manufacturerFinal', '$itemWeightFinal', '$sizeFinal', '$specialFeaturesFinal', '$colorFinal', '$brandFinal')";
     $result = $mysqli->query($sql);
 
     $productId = $mysqli->insert_id;
+    var_dump($productId);
 
     //ADD PRODCUT DESCRIPTION
     foreach($descriptions as $productDescription){
-        $productDescription = $mysqli->real_escape_string($productDescription);
+        $productDescription = trim($mysqli->real_escape_string($productDescription));
         $descriptionInsertSql = "INSERT INTO `product_description` (`product_id`, `description_name`)
                                  VALUES ($productId, '$productDescription')";
         $descriptionResult = $mysqli->query($descriptionInsertSql);
@@ -105,4 +424,3 @@ if($data->searchText){
         echo json_encode(["Result:" => "The Insert Query Done!"]);
     }
 }
-?>
--- index.php
+++ index.php
@@ -69,7 +69,7 @@ require 'vendor/autoload.php';
 // sleep(rand(1,5));
 $apiKey = '99f9a6f251cf4da6ab39fcb004ea08c9';
 //$linkToScrape = 'https://www.amazon.com/dp/B0CCK33WLB/ref=s9_acsd_al_bw_c2_x_3_t?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-11&pf_rd_r=AZJES0BMXEFS8HTC38SQ&pf_rd_t=101&pf_rd_p=98d75f35-c075-4148-beb3-9fa4081bd16a&pf_rd_i=1055398';
-$linkToScrape = 'https://www.amazon.com/Listerine-Anticavity-Mouthwash-Fluoride-Packaging/dp/B00495Q5OW/ref=sr_1_1?_encoding=UTF8&content-id=amzn1.sym.619b2b85-1519-44b0-b10f-193bd022e08c&pd_rd_r=35bc262c-7564-48d6-8f80-c6b8def56bc4&pd_rd_w=ZVnsk&pd_rd_wg=sDC5n&pf_rd_p=619b2b85-1519-44b0-b10f-193bd022e08c&pf_rd_r=WFQCWER3F9CCSPF5M0ZH&qid=1697984151&s=beauty-intl-ship&sr=1-1';
+$linkToScrape = 'https://www.amazon.ca/dp/B0CG8XJDP5/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B0CG8XJDP5&pd_rd_w=1vs95&content-id=amzn1.sym.d8c43617-c625-45bd-a63f-ad8715c2c055&pf_rd_p=d8c43617-c625-45bd-a63f-ad8715c2c055&pf_rd_r=WC49QT2Z74XGH5VNKTPV&pd_rd_wg=SHHNp&pd_rd_r=762fc926-5eef-4b6e-95fb-e9d968df3cdf&s=apparel&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWw';
 
 $client = new GuzzleHttp\Client();
 $response = $client->request('POST', 'https://api.zyte.com/v1/extract', [
@@ -85,44 +85,175 @@ $http_response_body = base64_decode($data->httpResponseBody);
 //var_dump($http_response_body);
 //Parsing Using DOMDocument
 $dom = new DOMDocument();
-//Load the HTML Content
+// Load the HTML Content
 @$dom->loadHTML($http_response_body);
 $xpath = new DOMXPath($dom);
 
-// $bodyValues = $xpath->query('//body/*');
-// var_dump($bodyValues);
-// foreach($bodyValues as $node){
-//     echo "Node Name: " . $node->nodeName . "\n";
-//     echo "Node Content: " . $node->textContent . "\n\n";
+// Initialize an array to store the scraped data
+$data = array();
+
+//====ECTACTING THE UL PRODUCT DETAILS=====
+// Access individual data items
+// $dateFirstAvailable = '';
+// $placeOfBusiness = '';
+// $asin = '';
+// $department = '';
+
+// $elements = $xpath->query('//div[@id="detailBullets_feature_div"]//li//span[@class="a-list-item"]');
+
+// foreach ($elements as $element) {
+//     $label = $element->getElementsByTagName('span')->item(0)->textContent;
+//     $value = $element->getElementsByTagName('span')->item(1)->textContent;
+
+//     // Clean up the label and value
+//     $label = trim($label);
+//     $value = trim($value);
+
+//     if (strpos($label, 'Date First Available') !== false) {
+//         $dateFirstAvailable = $value;
+//     } elseif (strpos($label, 'Place of Business') !== false) {
+//         $placeOfBusiness = $value;
+//     } elseif (strpos($label, 'ASIN') !== false) {
+//         $asin = $value;
+//     } elseif (strpos($label, 'Department') !== false) {
+//         $department = $value;
+//     }
 // }
-//Extract the Title of the Page
-//$divTitleSection = $xpath->query('//body//div[@id="titleSection"]');
 
-//Getting the The product title
-// $divTitleSection = $xpath->query('//body//div[@id="titleSection"]');
+// Extract the Description 1st structure
+$featureBullets = $dom->getElementById('feature-bullets');
+$descriptions = array(); 
 
-// if($divTitleSection->length > 0){
-//     $titleSection = $divTitleSection->item(0);
-//     $titleSectionContent = $titleSection->textContent;
-//     echo "The image goes here with its name: ".$titleSectionContent;
-// }else{
-//     echo "Title Section Not Found!";
-// }
+if ($featureBullets) {
+    $liItems = $featureBullets->getElementsByTagName('li');
+    foreach ($liItems as $liItem) {
+        // Get the first span element
+        $span = $liItem->getElementsByTagName('span')->item(0); 
+        if ($span) {
+            $description = $span->nodeValue;
+            $descriptions[] = $description; 
+        }
+    }
+} else {
+    echo "Description Not Found 1";
+}
+// Extract the Description from the 2nd structure
+$expander = $dom->getElementById('productFactsDesktopExpander');
 
-//Extract Image URL
-$imagTagWrapper = $xpath->query('//div[@class="imgTagWrapper"]');
-if($imagTagWrapper->length > 0){
-    $imageURL = $xpath->query('.//img/@src', $imagTagWrapper->item(0))->item(0)->nodeValue;
-    echo "<strong>THE IMAGE URL IS:</strong> ".$imageURL;
+if ($expander) {
+    $liItems = $expander->getElementsByTagName('li');
+
+    foreach ($liItems as $liItem) {
+        $span = $liItem->getElementsByTagName('span')->item(0);
+        if ($span) {
+            $description = $span->nodeValue;
+            $descriptions[] = $description;
+        }
+    }
+} else {
+    echo "Description Not Found 2!";
 }
 
-//Extracting the product title
-$titleSection = $xpath->query('//div[@id="titleSection"]');
-if($titleSection->length > 0){
-    $productTitle = $xpath->query('.//span[@id="productTitle"]', $titleSection->item(0))->item(0)->textContent;
-    echo "<br><br><strong> THE PRODUCT TITLE IS:</strong><br>". $productTitle;
-}
 
+
+// Now $descriptions contains the descriptions from the specific structure
+
+
+
+var_dump($descriptions);
+// Define the sections you want to scrape
+$sections = array(
+    'Technical Details' => 'productDetails_techSpec_section_1',
+    'Additional Information' => 'productDetails_detailBullets_sections1',
+);
+
+// Define the attributes you want to extract
+$attributes = array(
+    'Manufacturer',
+    'Item model number',
+    'Parcel Dimensions',
+    'ASIN',
+    'Fabric Type',
+    'Place of Business',
+    'Care Instructions',
+    'Batteries Required',
+    'Date First Available',
+    'Item Weight',
+    'Included components',
+    'Maximum Recommended Load',
+    'Size',
+);
+
+
+// Loop through the sections and attributes
+// foreach ($sections as $sectionName => $sectionId) {
+//     // Find the section by ID
+//     $section = $xpath->query("//*[@id='$sectionId']");
+
+//     if ($section->length > 0) {
+//         // Find the rows within the section
+//         $rows = $xpath->query(".//tr", $section->item(0));
+
+//         foreach ($rows as $row) {
+//             // Extract the header and data cells
+//             $header = trim($xpath->query(".//th", $row)->item(0)->textContent);
+//             $dataCell = trim($xpath->query(".//td", $row)->item(0)->textContent);
+
+//             // Check if the header is in the list of attributes you want to scrape
+//             if (in_array($header, $attributes)) {
+//                 $data[$sectionName][$header] = $dataCell;
+//             }
+//         }
+//     }
+// }
+
+//---------------------------WORKIND UL PRODUCT DETAILS----------------------------
+
+// Scenario 2: Check if product details are in a list (ul structure)
+// $ulSections = $xpath->query("//div[@id='detailBulletsWrapper_feature_div']//ul[contains(@class, 'a-unordered-list')]");
+// foreach ($ulSections as $ul) {
+//     $listItems = $xpath->query(".//li", $ul);
+//     foreach ($listItems as $listItem) {
+//         $dataElements = $xpath->query(".//span[contains(@class, 'a-text-bold')]", $listItem);
+
+//         if ($dataElements->length === 1) {
+//             $header = trim($dataElements->item(0)->textContent);
+//             $value = trim($listItem->textContent);
+
+//             // Remove the header text from the value
+//             $value = str_replace($header, '', $value);
+
+//             $data['Product Details'][$header] = $value;
+//         }
+//     }
+// }
+//-----------------try new ul scraping--------------------
+
+// Now, $data should contain the scraped product details
+//var_dump($data);
+// $technicalDetails = $data['Technical Details'];
+// $additionalInformation = $data['Additional Information'];
+
+// Specific attributes you want to echo
+// echo "Item Model Number: " . $technicalDetails['Item model number'] . "<br>";
+// echo "Parcel Dimensions: " . $technicalDetails['Parcel Dimensions'] . "<br>";
+// echo "ASIN: " . $additionalInformation['ASIN'] . "<br>";
+// echo "Date First Available: " . $additionalInformation['Date First Available'] . "<br>";
+// echo "Manufacturer: " . $additionalInformation['Manufacturer'] . "<br>";
+// // And so on for other attributes
+// $productDetails = $data['Product Details'];
+
+// Specific attributes you want to echo
+// echo "Parcel Dimensions: " . $productDetails['Parcel Dimensions  : '] . "<br>";
+// echo "Date First Available: " . $productDetails['Date First Available  : '] . "<br>";
+// echo "Manufacturer: " . $productDetails['Manufacturer  : '] . "<br>";
+// echo "Place of Business: " . $productDetails['Place of Business  : '] . "<br>";
+// echo "ASIN: " . $productDetails['ASIN  : '] . "<br>";
+// echo "Item Model Number: " . $productDetails['Item model number  : '] . "<br>";
+// And so on for other attributes
+
+
+//----------------------END PRODUCT UL---------------------------------------------
 //Extract The Descriptions
 // $featureBullets = $xpath->query('//div[@id="feature-bullets"]');
 // $descriptions = [];
@@ -135,28 +266,26 @@ if($titleSection->length > 0){
 // }
 // echo "Descriptions: " . implode("\n", $descriptions);
 
-$featureBullets = $dom->getElementById('feature-bullets');
-$liItems = $featureBullets->getElementsByTagName('li');
+//----------------------------- WORKIND SCRAPE ------------------------------------------
 
-$descriptions = array(); // Initialize an array to store the descriptions
+// Extracting Brand Name
+// $brandNameElement = $xpath->query('//tr[contains(@class, "po-brand")]//span[@class="a-size-base po-break-word"]')->item(0);
+// $brandName1 = $brandNameElement ? $brandNameElement->textContent : "N/A";
 
-foreach ($liItems as $liItem) {
-    $span = $liItem->getElementsByTagName('span')[0]; // Get the first span element
-    if ($span) {
-        $description = $span->nodeValue;
-        $descriptions[] = $description; // Store the description in the array
-    }
-}
+// Extracting Model Name
+// $colorElement = $xpath->query('//tr[contains(@class, "po-color")]//span[@class="a-size-base po-break-word"]')->item(0);
+// $color1 = $colorElement ? $colorElement->textContent : "N/A";
 
-// To echo all descriptions using a foreach loop
-echo "<br><strong>PRODUCT DESCRIPTION:</strong><br>";
-foreach ($descriptions as $description) {
-    echo "<br>- ".$description . PHP_EOL; // Use PHP_EOL for a newline between descriptions
-}
+// Extracting Special Feature
+// $specialFeatureElement = $xpath->query('//tr[contains(@class, "po-special_feature")]//span[@class="a-size-base po-break-word"]')->item(0);
+// $specialFeature = $specialFeatureElement ? $specialFeatureElement->textContent : "N/A";
 
-// To access a specific description by index (e.g., the first description)
-if (isset($descriptions[0])) {
-    echo "First description: " . $descriptions[0];
-}
+// Extracting Age Range
+// $ageRangeElement = $xpath->query('//tr[contains(@class, "po-age_range_description")]//span[@class="a-size-base po-break-word"]')->item(0);
+// $ageRange = $ageRangeElement ? $ageRangeElement->textContent : "N/A";
 
+// Extracting Width Height
+// $dimensionElement = $xpath->query('//tr[contains(@class, "po-item_depth_width_height")]//span[@class="a-size-base po-break-word"]')->item(0);
+// $dimensionElement = $dimensionElement ? $dimensionElement->textContent : "N/A";
+//-------------------------------------END OF WORKING--------------------------------------------
 ?>
