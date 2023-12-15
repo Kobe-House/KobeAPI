43f6a8d5013014d1bf950857246bf477b469d307
Tom Amundala <100129520+toma2k@users.noreply.github.com>
2023-12-15 10:06:47 +0200

    Prod Push

--- database/query/scraping/add/index.php
+++ database/query/scraping/add/index.php
@@ -4,15 +4,27 @@
     ini_set('display_errors', '1');
 
     //Setting Headers for Cross Origin Resource Sharing
-    //header("Access-Control-Allow-Origin: *");
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        // Return only the headers and not the content
+        //header('Access-Control-Allow-Origin: http://localhost:3000');
+        header("Access-Control-Allow-Origin: http://sellerzone.io");
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: Authorization, Content-Type');
+        header('Content-Type: application/json;');
+        exit(0);
+    }
+    // Regular request processing
+    //header('Access-Control-Allow-Origin: http://localhost:3000');
     header("Access-Control-Allow-Origin: http://sellerzone.io");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: Authorization, Content-Type');
+    header('Content-Type: application/json;');
 
     //Database Connection
     include('../../../migrations/2023083000000_scraping_bulk_kobe.php');
     $connection = new Connection();
     $mysqli = $connection->getConnection();
+
 
     // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
     // $dotenv->load();
@@ -23,6 +35,10 @@
     include '../../dynamics/amazon-script.php';
     include '../../dynamics/bestbuy-script.php';
     include '../../dynamics/walmart-script.php';
+
+    //Firebase to decode the token and get claims
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
 
     $apiKey = '76c01917efb5461fb2f23e6ab7551885';
 
@@ -35,7 +51,7 @@
     //-------Getting the URL--------
     $scrapingURL = $data->searchText;
     //-------Get Guid----------------
-    $guid = $data->guid;
+    //  $guid = $data->guid;
 
     //Implementing Guzzle
     $client = new GuzzleHttp\Client();
@@ -57,24 +73,46 @@
     @$dom->loadHTML($http_response_body);
     $xpath = new DOMXPath($dom);
 
-    /* -------------------------------------------------------------------------- */
-    /*                                   Amazon                                   */
-    /* -------------------------------------------------------------------------- */
+    if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+        $authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+        preg_match('/Bearer\s(\S+)/', $authHeader, $matches);
+        if (isset($matches[1])) {
+            $token = $matches[1];
+            $urufunguzo = "4d46e827c8c29048f1b3de28123ed620fee5f1ab68d5fad46e57c7b3b3e66e45";
+            try {
+                $decode = JWT::decode($token, new Key($urufunguzo, 'HS512'));
+                $guid = $decode->guid;
+                if ($guid) {
+                    /* -------------------------------------------------------------------------- */
+                    /*                                   Amazon                                   */
+                    /* -------------------------------------------------------------------------- */
 
-    if ($source == 'amazon') {
-        require "amazon-add.php";
-    }
+                    if ($source == 'amazon') {
+                        require "amazon-add.php";
+                    }
 
-    /* -------------------------------------------------------------------------- */
-    /*                                   Walmart                                  */
-    /* -------------------------------------------------------------------------- */
-    if ($source == 'walmart') {
-        require "walmart-add.php";
-    }
+                    /* -------------------------------------------------------------------------- */
+                    /*                                   Walmart                                  */
+                    /* -------------------------------------------------------------------------- */
+                    if ($source == 'walmart') {
+                        require "walmart-add.php";
+                    }
 
-    /* -------------------------------------------------------------------------- */
-    /*                                   Bestbuy                                  */
-    /* -------------------------------------------------------------------------- */
-    if ($source == 'bestbuy') {
-        require "bestbuy-add.php";
+                    /* -------------------------------------------------------------------------- */
+                    /*                                   Bestbuy                                  */
+                    /* -------------------------------------------------------------------------- */
+                    if ($source == 'bestbuy') {
+                        require "bestbuy-add.php";
+                    }
+                } else {
+                    echo json_encode("No Guid Found");
+                }
+            } catch (Exception $e) {
+                echo json_encode(["message" => "Error" . $e->getMessage()]);
+            }
+        } else {
+            echo "NOT FOUND";
+        }
+    } else {
+        http_response_code(401);
     }
--- database/query/scraping/get/index.php
+++ database/query/scraping/get/index.php
@@ -1,39 +1,40 @@
 <?php
 
 // Reporting All Errors 
-
-use GuzzleHttp\Promise\Is;
-
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 //Setting Headers for Cross Origin Resource Sharing
-//header("Access-Control-Allow-Origin: *");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // Return only the headers and not the content
+    //header('Access-Control-Allow-Origin: http://localhost:3000');
+    header("Access-Control-Allow-Origin: http://sellerzone.io");
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: Authorization, Content-Type');
+    header('Content-Type: application/json;');
+    exit(0);
+}
+// Regular request processing
+//header('Access-Control-Allow-Origin: http://localhost:3000');
 header("Access-Control-Allow-Origin: http://sellerzone.io");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: Authorization, Content-Type');
+header('Content-Type: application/json;');
+
+//---  Autoloads -----
+require '../../../../vendor/autoload.php';
+
+//Firebase to decode the token and get claims
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 //Database Connection
 include('../../../migrations/2023083000000_scraping_bulk_kobe.php');
 $connection = new Connection();
 $mysqli = $connection->getConnection();
-
-//---- SELECT QUERY -----
-$sql = "SELECT p.product_id, p.source, p.created_at, p.title, p.image_url, p.item_model, p.parcel_dimensions, p.asin, p.manufacturer, p.item_weight, p.size, p.special_features, p.color, p.brand, pd.product_description_id, pd.description_name, pi.product_images_id, pi.product_image_url
-        FROM product p
-        LEFT JOIN product_description pd ON p.product_id = pd.product_id
-        LEFT JOIN product_images pi ON p.product_id = pi.product_id
-        ORDER BY 
-        p.created_at DESC,
-        p.product_id DESC,
-        pd.product_description_id DESC,
-        pi.product_images_id DESC";
-
-$result = $mysqli->query($sql);
-
-if (!$result) {
-    echo json_encode(["Error:" => $mysqli->error]);
-} else {
+//---- Processing Query Result Function --------
+function processProducts($result)
+{
     $tempProducts = [];
 
     while ($data = $result->fetch_assoc()) {
@@ -85,4 +86,62 @@ if (!$result) {
     } else {
         echo json_encode($finalProducts);
     }
+}
+
+$addon = "";
+$sql = "SELECT p.product_id, p.user_guid, p.source, p.created_at, p.title, p.image_url, p.item_model, p.parcel_dimensions, p.asin, p.manufacturer, p.item_weight, p.size, p.special_features, p.color, p.brand, pd.product_description_id, pd.description_name, pi.product_images_id, pi.product_image_url
+        FROM product p
+        LEFT JOIN product_description pd ON p.product_id = pd.product_id
+        LEFT JOIN product_images pi ON p.product_id = pi.product_id";
+
+if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+    $authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+    preg_match('/Bearer\s(\S+)/', $authHeader, $matches);
+    if (isset($matches[1])) {
+        $token = $matches[1];
+        $urufunguzo = "4d46e827c8c29048f1b3de28123ed620fee5f1ab68d5fad46e57c7b3b3e66e45";
+        try {
+            $decode = JWT::decode($token, new Key($urufunguzo, 'HS512'));
+            $access = $decode->level;
+            $guid = $decode->guid;
+            if ($access == "2") {
+                $sql .= " ORDER BY 
+                            p.created_at DESC,
+                            p.product_id DESC,
+                            pd.product_description_id DESC,
+                            pi.product_images_id DESC";
+                $result = $mysqli->query($sql);
+                if (!$result) {
+                    echo json_encode(["Error:" => $mysqli->error]);
+                } else {
+                    processProducts($result);
+                }
+            } elseif ($access == "0") {
+                $addon = " WHERE p.user_guid = '$guid'";
+                $sql .= $addon;
+
+                $sql .= " ORDER BY 
+                            p.created_at DESC,
+                            p.product_id DESC,
+                            pd.product_description_id DESC,
+                            pi.product_images_id DESC";
+                $result = $mysqli->query($sql);
+
+                if (!$result) {
+                    echo json_encode(["Error:" => $mysqli->error]);
+                } else {
+                    processProducts($result);
+                }
+            } else {
+                echo json_encode(http_response_code(401));
+            }
+        } catch (Exception $e) {
+            echo json_encode(["message" => "Error: " . $e->getMessage()]);
+        }
+    } else {
+        echo "NOT FOUND";
+    }
+} else {
+    http_response_code(401);
+    echo json_encode("BYANZE");
 }
